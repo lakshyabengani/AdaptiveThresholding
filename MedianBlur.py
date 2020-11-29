bddc5975c23f11eae6101f9c5bb68ac046c29e85
Lakshya Bengani <lakshyabengani14@gmail.com>
2020-11-29 11:59:54 +0530

    Median Filter for coloured images done

--- MedianBlur.py
+++ MedianBlur.py
@@ -1,10 +1,19 @@
+'''
+MEDIAN BLUR/FILTER : 
+
+This algorithm filter considers each pixel in the image in turn and looks at its 
+nearby neighbors to decide whether or not it is representative of its surroundings.
+
+'''
 import numpy as np
 from PIL import Image
 
+# Create a window of specific size x
 def create_window(x):
     window = np.zeros((1,x**2))
     return window
 
+# Caluclates the output pixel value for the provided pixel in the input image
 def slide_window(arr,i,j,k,window):
     if i<k or j<k or i+k >= arr.shape[0] or j+k >= arr.shape[1]:
         return arr[i][j]
@@ -18,25 +27,38 @@ def slide_window(arr,i,j,k,window):
     	x = window.shape[0]*window.shape[1]
     	return window[0][x//2]
 
+# This function performs the Median Blur operation on a single clour channel and returns the output array for that channel
 def MedianBlur(arr,window):
     k = np.sqrt(window.shape[1])
     k = int(k//2)
     new_arr = np.zeros(arr.shape)
+
+    # for each pixel calculate the pixel for the blurred image 
     for i in range(arr.shape[0]):
-	for j in range(arr.shape[1]):
-		new_arr[i][j] = slide_window(arr,i,j,k,window)
+	    for j in range(arr.shape[1]):
+		    new_arr[i][j] = slide_window(arr,i,j,k,window)
     new_arr.astype('uint8')
+
     return new_arr
 
 if __name__ == "__main__":
-    img = Image.open('original.jpg')
+    img = Image.open('hacking.jpg')
     arr = np.array(img)
+
+    # define the window size and create a window
     window = create_window(5)
+
+    # create a new numpy array with the same shape as that of the pic
+    # this is serve as the array fo the new pic 
     new_arr = np.zeros(arr.shape,dtype='uint8')
-    n_arr = MedianBlur(arr,window)
-    for i in range(n_arr.shape[0]):
-        for j in range(n_arr.shape[1]):
-            new_arr[i][j] = int(n_arr[i][j])
+
+    # for each colour channel perform Median Blur
+    for k in range(arr.shape[2]):
+        n_arr = MedianBlur(arr[:,:,k],window)
+        for i in range(n_arr.shape[0]):
+            for j in range(n_arr.shape[1]):
+                new_arr[i][j][k] = int(n_arr[i][j])
+
+    # Combine the results from all the channels  and save the pic 
     new_img = Image.fromarray(new_arr)
-    new_img.save('MeadianBlur.jpg')
-    
+    new_img.save('MeadianBlur.jpg')    
